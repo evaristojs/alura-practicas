6c280d606ecf415f5172d473e9ce4c8e5680c158
evaristo <evaristoperez533@gmail.com>
2025-02-06 00:08:58 -0400

    Crea una función que muestre una alerta con el mensaje: "Yo amo JS" siempre que se presione el botón "Alerta".

--- Desafio-1-interactuando-con-HTML/app.js
+++ Desafio-1-interactuando-con-HTML/app.js
@@ -3,9 +3,13 @@ titulo.innerHTML = 'Hora del Desafío';
 
 function mensajeConsola() {
     console.log("El boton fue presionado");
-}
+};
 
 function nombreCiudad() {
     let nombreCiudad = prompt("Ingrese el nombre de una ciudad de Brasil");
     alert(`Estuve en ${nombreCiudad} y me acordé de ti`);
-}
+};
+
+function mensajeAlerta() {
+    alert("Yo amo a JavaScript");
+};
