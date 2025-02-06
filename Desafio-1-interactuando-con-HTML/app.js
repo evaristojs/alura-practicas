b8eddcd80e431f4b41ffcd3f7e8b528a02a43ce7
evaristo <evaristoperez533@gmail.com>
2025-02-06 00:06:59 -0400

    Crea una función que se ejecute cuando se haga clic en el botón "prompt", preguntando el nombre de una ciudad de Brasil. Luego, muestra una alerta con el mensaje concatenando la respuesta con el texto: "Estuve en {ciudad} y me acordé de ti".

--- Desafio-1-interactuando-con-HTML/app.js
+++ Desafio-1-interactuando-con-HTML/app.js
@@ -4,3 +4,8 @@ titulo.innerHTML = 'Hora del Desafío';
 function mensajeConsola() {
     console.log("El boton fue presionado");
 }
+
+function nombreCiudad() {
+    let nombreCiudad = prompt("Ingrese el nombre de una ciudad de Brasil");
+    alert(`Estuve en ${nombreCiudad} y me acordé de ti`);
+}
